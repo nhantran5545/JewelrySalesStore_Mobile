3c51afadeec158f668a6e752209cd4268f546379
Trần Trọng Nhân <93432020+nhantran5545@users.noreply.github.com>
2024-05-17 20:21:16 +0700

    Create sample.tsx

---
+++ src/api/sample.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class sample extends Component {
+  render() {
+    return (
+      <View>
+        <Text>sample</Text>
+      </View>
+    )
+  }
+}
